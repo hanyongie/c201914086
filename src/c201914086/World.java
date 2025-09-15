538b1a2b7e325d6804d26cdc3526ef1aba9a96d3
hanyongie <lasd8422@gmail.com>
2025-09-15 23:15:13 +0900

    five

--- src/c201914086/World.java
+++ src/c201914086/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void run(){
         System.out.println("201914086");
     }
+
+    public static void run2(){
+        System.out.println("hello world");
+    }
 }
